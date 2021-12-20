3fe310f70063b9d96a637b7f3f264f8b278d170e
haero_kim <mac-pro@kakao.com>
2021-12-20 17:13:38 +0900

    Using Column

--- app/src/main/java/com/haero_kim/composepractice/MainActivity.kt
+++ app/src/main/java/com/haero_kim/composepractice/MainActivity.kt
@@ -3,7 +3,7 @@ package com.haero_kim.composepractice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -26,22 +26,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Surface(color = MaterialTheme.colors.primary) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    Surface(
+        color = MaterialTheme.colors.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+    ) {
+        // 컴포즈 컴포넌트를 열 (Column) 모양으로 배치
+        Column(modifier = Modifier
+            .fillMaxWidth()
+            .padding(24.dp)) {
+            Text(text = "Hello, ")
+            Text(text = name)
+        }
     }
 }
 
 @Composable
-private fun MyApp() {
-    Surface(color = MaterialTheme.colors.background) {
-        Greeting("Android")
+private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+    Column(modifier = Modifier.padding(vertical = 4.dp)) {
+        for (name in names) {
+            Greeting(name = name)
+        }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        Greeting("Android")
+        MyApp()
     }
 }
